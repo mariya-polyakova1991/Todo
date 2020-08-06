e4daf00cb693eee24afddd9cf3eb446be96166bf
mariya-polyakova1991 <mariyaluk@yandex.ru>
2020-08-06 09:54:34 +0300

    fix js

--- js/todo.js
+++ js/todo.js
@@ -2,44 +2,50 @@ const input = document.getElementById("task");
 const buttonAdd = document.getElementById("add");
 const list = document.getElementById("ol");
 
-// function Task() {
-//   this.value = value;
-// }
-// Task.prototype.add = function () {
-//   if (!this.value == "") {
+function Task(value) {
+  this.value = value;
+  this.add = function () {
+    if (!this.value == "") {
+      let li = document.createElement("li");
+      li.className = "alert";
+      li.innerHTML = input.value;
+      list.prepend(li);
+    }
+  };
+}
+
+buttonAdd.addEventListener("click", function () {
+  const massage = new Task(input.value);
+  massage.add();
+  input.value = "";
+});
+document.addEventListener("keydown", function (evt) {
+  if ((evt.keyCode === 13) & (!input.value == "")) {
+    const massage = new Task(input.value);
+    massage.add();
+    input.value = "";
+  }
+});
+
+// buttonAdd.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   if (!input.value == "") {
+//     console.log("=======button====");
 //     let li = document.createElement("li");
 //     li.className = "alert";
 //     li.innerHTML = input.value;
 //     list.prepend(li);
-//     this.value = "";
+//     input.value = "";
 //   }
-// };
-
-// buttonAdd.addEventListener("click", function () {
-//   const massage = new Task(input.value);
-//   massage.add();
-//   console.log("=======rfgh====");
 // });
 
-buttonAdd.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  if (!input.value == "") {
-    console.log("=======button====");
-    let li = document.createElement("li");
-    li.className = "alert";
-    li.innerHTML = input.value;
-    list.prepend(li);
-    input.value = "";
-  }
-});
-
-document.addEventListener("keydown", function (evt) {
-  if ((evt.keyCode === 13) & (!input.value == "")) {
-    console.log("=======button====");
-    let li = document.createElement("li");
-    li.className = "alert";
-    li.innerHTML = input.value;
-    list.prepend(li);
-    input.value = "";
-  }
-});
+// document.addEventListener("keydown", function (evt) {
+//   if ((evt.keyCode === 13) & (!input.value == "")) {
+//     console.log("=======button====");
+//     let li = document.createElement("li");
+//     li.className = "alert";
+//     li.innerHTML = input.value;
+//     list.prepend(li);
+//     input.value = "";
+//   }
+// });
