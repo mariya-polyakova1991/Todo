d1af32ae45fc3cc8efc781c91b4de1f7716bcc70
mariya-polyakova1991 <mariyaluk@yandex.ru>
2020-08-06 08:54:19 +0300

    add js

---
+++ js/todo.js
@@ -0,0 +1,34 @@
+const input = document.getElementById("task");
+const buttonAdd = document.getElementById("add");
+const list = document.getElementById("ol");
+
+// function Task() {
+//   this.value = value;
+// }
+// Task.prototype.add = function () {
+//   if (!this.value == "") {
+//     let li = document.createElement("li");
+//     li.className = "alert";
+//     li.innerHTML = input.value;
+//     list.prepend(li);
+//     this.value = "";
+//   }
+// };
+
+// buttonAdd.addEventListener("click", function () {
+//   const massage = new Task(input.value);
+//   massage.add();
+//   console.log("=======rfgh====");
+// });
+
+buttonAdd.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (!input.value == "") {
+    console.log("=======button====");
+    let li = document.createElement("li");
+    li.className = "alert";
+    li.innerHTML = input.value;
+    list.prepend(li);
+    input.value = "";
+  }
+});
