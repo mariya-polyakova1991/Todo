58c3e4d0e6b31524d5bb28b1b9cf4d9ca2baf72a
mariya-polyakova1991 <mariyaluk@yandex.ru>
2020-09-07 13:47:04 +0300

    fix cod styles

--- js/styles.js
+++ js/styles.js
@@ -1,40 +1,50 @@
-function fixImage(img) {
-  document.body.style.backgroundImage = img;
-  menu.classList.remove("menu__nav__open");
+function FixImage() {
+  this.img =
+    JSON.parse(localStorage.getItem("imgSave")) || "url(../Images/214588.jpg)";
 }
 
-function fixColor(color) {
+FixImage.prototype.fixImage = function (img) {
+  document.body.style.backgroundImage = img;
+  menu.classList.remove("menu__nav__open");
+  const imgSave = localStorage.setItem("Img", JSON.stringify(img));
+};
+FixImage.prototype.fixColor = function (color) {
   document.body.style.backgroundColor = color;
   document.body.style.backgroundImage = "none";
 
   menu.classList.remove("menu__nav__open");
-}
+};
 
 panda.addEventListener("click", function () {
-  fixImage("url(../Images/10.jpg)");
+  img = "url(../Images/10.jpg)";
+  ImgController.fixImage(img);
 });
 
 rainbow.addEventListener("click", function () {
-  fixImage("url(../Images/rainbow-rain-1.jpg)");
+  ImgController.fixImage("url(../Images/rainbow-rain-1.jpg)");
 });
 forest.addEventListener("click", function () {
-  fixImage("url(../Images/150502878459b4eab0107551.22903829.jpg)");
+  ImgController.fixImage(
+    "url(../Images/150502878459b4eab0107551.22903829.jpg)"
+  );
 });
 
 space.addEventListener("click", function () {
-  fixImage("url(../Images/1581997163.jpg)");
+  ImgController.fixImage("url(../Images/1581997163.jpg)");
 });
 
 ice.addEventListener("click", function () {
-  fixImage("url(../Images/214588.jpg)");
+  ImgController.fixImage("url(../Images/214588.jpg)");
 });
 
 black.addEventListener("click", function () {
-  fixColor("black");
+  ImgController.fixColor("black");
 });
 blue.addEventListener("click", function () {
-  fixColor("royalblue");
+  ImgController.fixColor("royalblue");
 });
 pink.addEventListener("click", function () {
-  fixColor("pink");
+  ImgController.fixColor("pink");
 });
+
+var ImgController = new FixImage();
